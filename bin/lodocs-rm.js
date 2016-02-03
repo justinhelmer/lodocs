0b256b1fa7276bc3f647a141640acc879c41e36b
justinhelmer <jhelmer25@gmail.com>
2016-02-02 20:48:42 -0800

    handle process exit

--- bin/lodocs-rm.js
+++ bin/lodocs-rm.js
@@ -2,6 +2,9 @@
 (function() {
   'use strict';
 
+  var chalk = require('chalk');
+  var cjsErr = require('commander.js-error');
+  var grm = require('gh-release-manager');
   var path = require('path');
   var program = require('commander');
   var open = require('../lib/open');
@@ -11,7 +14,7 @@
       .option('-v, --verbose', 'output additional information; takes precedence over `-q')
       .parse(process.argv);
 
-  require('gh-release-manager')(program.args[0] || 'suite', {
+  var options = {
     paths: {
       docs: path.resolve(__dirname, '../docs'),
       jsdoc: path.resolve(__dirname, '../node_modules/.bin/jsdoc'),
@@ -19,7 +22,25 @@
     },
     quiet: program.quiet,
     verbose: program.verbose
-  });
+  };
+
+  grm(program.args[0] || 'suite', options)
+      .then(function() {
+        console.log('\n' + chalk.green('Done with everything.'));
+        process.exit();
+      })
+      .catch(function(err) {
+        if (!program.quiet) {
+          cjsErr(err.message);
+        }
+
+        if (program.verbose) {
+          console.trace();
+        }
+
+        process.exit(1);
+      })
+      .done();
 
   require('node-clean-exit')({verbose: program.verbose});
 })();
