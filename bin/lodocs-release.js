9276d8accbb22b9b85f1bd91ec2be53b20617323
justinhelmer <jhelmer25@gmail.com>
2016-02-03 16:06:54 -0800

    expose all of github release manager's available options

--- bin/lodocs-release.js
+++ bin/lodocs-release.js
@@ -2,6 +2,7 @@
 (function() {
   'use strict';
 
+  var _ = require('lodash');
   var chalk = require('chalk');
   var cjsErr = require('commander.js-error');
   var grm = require('gh-release-manager');
@@ -10,17 +11,17 @@
   var open = require('../lib/open');
 
   program
+      .description('Description:\n\n    Download releases, generate documentation, build site, deploy.')
+      .option('-o, --opts [path]', 'the path to a conf file; cli args take precedence')
+      .option('-d, --docs [path]', 'the path to output documentation; defaults to ../docs')
+      .option('-k, --keep [path]', 'the path to keep releases; else they are removed')
+      .option('-p, --path [path]', 'release-relative path to the JSDoc file; assumes index.js')
       .option('-q, --quiet', 'output nothing (suppress STDOUT and STDERR)')
-      .option('-v, --verbose', 'output additional information; takes precedence over `-q')
+      .option('-r, --recent <n>', 'only parse documentation for the <n> most recent releases')
+      .option('-v, --verbose [n]', 'true for more output; higher number (ie 2) for even more', false)
       .parse(process.argv);
 
-  var options = {
-    docs: path.resolve(__dirname, '../docs'),
-    quiet: program.quiet,
-    verbose: program.verbose
-  };
-
-  grm('release', options)
+  grm('release', _.pick(program, ['docs', 'keep', 'opts', 'path', 'quiet', 'recent', 'verbose']))
       .then(function() {
         console.log('\n' + chalk.green('Done with everything.'));
         process.exit();
