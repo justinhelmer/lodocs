1c576428b0e13d34859ebb962a1826daf8766875
justinhelmer <jhelmer25@gmail.com>
2016-02-05 16:57:46 -0800

    offload linting responsibility
    - grab latest minor version of gh-release-manager
    - remove all linting references
    - set up eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,11 @@
   'use strict';
 
   var gulp = require('gulp');
-  var jscs = require('gulp-jscs');
-  var jshint = require('gulp-jshint');
   var path = require('path');
 
   // All task definitions
   gulp.task('build', ['copyCSS']);
-  gulp.task('lint', lint);
-  gulp.task('copyCSS', ['lint'], copyCSS);
+  gulp.task('copyCSS', copyCSS);
 
   // Set up the build path relative to the working directory
   var buildPath = path.resolve('_site');
@@ -23,17 +20,5 @@
     ];
 
     return gulp.src(files).pipe(gulp.dest(buildPath + '/css'));
-  }
-
-  function lint() {
-    var files = ['bin/*.js', 'lib/*.js', 'gulpfile.js'];
-
-    return gulp
-        .src(files)
-        .pipe(jshint())
-        .pipe(jscs())
-        .pipe(require('gulp-jscs-stylish').combineWithHintResults())
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(jshint.reporter('fail'));
   }
 })();
