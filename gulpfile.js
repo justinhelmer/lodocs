838a50776e57f13210877d1305e7924d74881799
justinhelmer <jhelmer25@gmail.com>
2016-02-08 14:21:17 -0800

    prefer normalize.scss to normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -8,17 +8,18 @@
   gulp.task('build', ['copyCSS']);
   gulp.task('copyCSS', copyCSS);
 
-  // Set up the build path relative to the working directory
-  var buildPath = path.resolve('_site');
+  // Set up the src path relative to the working directory
+  var srcPath = path.resolve('source');
 
   // Change working directory to the location of the gulpfile, else gulp will not work correctly
   process.chdir(path.resolve(__dirname));
 
   function copyCSS() {
     var files = [
-      'node_modules/normalize.css/normalize.css'
+      'node_modules/normalize-scss/sass/**',
+      'node_modules/support-for/sass/_support-for.scss'
     ];
 
-    return gulp.src(files).pipe(gulp.dest(buildPath + '/css'));
+    return gulp.src(files).pipe(gulp.dest(srcPath + '/css'));
   }
 })();
