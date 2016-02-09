8cbe71568dd63b70e8da557c9ab50931e14a30f7
justinhelmer <jhelmer25@gmail.com>
2016-02-08 18:37:20 -0800

    integrate with the github release manager serve functionality
    - get onto latest branch of GRM (0.6.x)
    - proxy server to GRM
    - all jekyll/ruby dependencies now completely removed
    - bump lodocs program and package to 0.6.x branch

--- bin/lodocs-serve.js
+++ bin/lodocs-serve.js
@@ -2,28 +2,6 @@
 (function() {
   'use strict';
 
-  var program = require('commander');
-  var spork = require('spork');
-  var open = require('../lib/open');
-
-  program
-      .option('-q, --quiet', 'output nothing (suppress STDOUT and STDERR)')
-      .option('-v, --verbose', 'output additional information; takes precedence over `-q')
-      .option('-o, --open <n>', 'launch the site in a browser `n` seconds after it begins being served')
-      .parse(process.argv);
-
-  var port = program.args[0] || '4000';
-  var args = ['serve', '--port', port];
-
-  if (program.open) {
-    open(program.open, '', port);
-  }
-
-  if (program.verbose && !program.quiet) {
-    args.push('--verbose');
-  }
-
-  spork('jekyll', args);
-  require('node-clean-exit')({quiet: program.quiet, verbose: program.verbose});
+  require('gh-release-manager').cli('serve', 'Serve the lodash website locally for debugging / development.');
 })();
 
